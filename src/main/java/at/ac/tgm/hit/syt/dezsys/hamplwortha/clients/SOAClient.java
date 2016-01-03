f7a5208e218fafc364d84c032b5edcc8849b3bcd
swortha-tgm <swortha@student.tgm.ac.at>
2016-01-03 18:15:45 +0100

    added SOAP CLient

---
+++ src/main/java/at/ac/tgm/hit/syt/dezsys/hamplwortha/clients/SOAClient.java
@@ -0,0 +1,84 @@
+package at.ac.tgm.hit.syt.dezsys.hamplwortha.clients;
+
+import javax.xml.soap.*;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SOAClient {
+
+    private static final String NAMESPACE = "http://at/ac/tgm/hit/syt/dezsys/hamplwortha/soa";
+
+    public static void main(String args[]) {
+        System.out.print("search for: ");
+        String title = "";
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            title = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // Create SOAP Connection
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+            // Send SOAP Message to SOAP Server
+            String url = "http://localhost:8000/knowledge/search";
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(title), url);
+
+            // Process the SOAP Response
+            printSOAPResponse(soapResponse);
+
+            soapConnection.close();
+        } catch (Exception e) {
+            System.err.println("Error occurred while sending SOAP Request to Server");
+            e.printStackTrace();
+        }
+    }
+
+    private static void printSOAPResponse(SOAPMessage soapResponse) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        Source sourceContent = soapResponse.getSOAPPart().getContent();
+        System.out.print("\nResponse SOAP Message = ");
+        StreamResult result = new StreamResult(System.out);
+        transformer.transform(sourceContent, result);
+    }
+
+    private static SOAPMessage createSOAPRequest(String title) throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        envelope.addNamespaceDeclaration("knowledge", NAMESPACE);
+
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement requestElem = soapBody.addChildElement("getKnowledgeRequest", "knowledge");
+        SOAPElement titleElem = requestElem.addChildElement("title", "knowledge");
+        titleElem.addTextNode(title);
+
+        // SOAP Headers
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", NAMESPACE  + "getKnowledgeRequest");
+
+        soapMessage.saveChanges();
+
+        // Print the request message
+        System.out.print("Request SOAP Message = ");
+        soapMessage.writeTo(System.out);
+        System.out.println();
+
+        return soapMessage;
+    }
+}
